d291ff7486ac389c7ebb9e6646cdff7707686f51
Eric Larson <ewlarson@umn.edu>
2018-01-17 10:31:14 -0600

    Opps, put jobs in app/jobs

--- lib/generators/geoblacklight/jobs_generator.rb
+++ lib/generators/geoblacklight/jobs_generator.rb
@@ -11,7 +11,7 @@ module Geoblacklight
     EOS
 
     def create_store_image_jobs
-      copy_file "jobs/store_image_job.rb", "jobs/store_image_job.rb"
+      copy_file "jobs/store_image_job.rb", "app/jobs/store_image_job.rb"
     end
   end
 end
